c7cc7d7080f63bfa8ded41f524e185bd0a9b5be4
Dean Alan Summer <59782978+JavascriptLearner815@users.noreply.github.com>
2022-02-21 02:36:40 +0000

    Working game

---
+++ main.py
@@ -0,0 +1,21 @@
+player = {
+    "hp": 20,
+    "maxhp": 20
+}
+valid_hp_number = [1,2,3,4,5]
+
+def game():
+    if player["hp"] <= 0:
+        return print("You died!")
+
+    # decrease_hp = input(f"Your HP is {player['hp']}. Decrease HP? ")
+
+    # if decrease_hp == "yes":
+        # decrease_amount = int(input("How much? (1-5) "))
+        # if decrease_amount in valid_hp_number:
+            # player["hp"] -= decrease_amount
+        # else:
+            # print("You can only decrease your HP by 1-5!")
+    game() # Keep running the game
+
+game()
